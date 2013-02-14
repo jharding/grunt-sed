2f062605ae5f32351e48e9d437a6fbc4c35930d0
Jake Harding <jharding@twitter.com>
2013-02-14 10:41:55 -0800

    Add tests.

---
+++ test/Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sed: {
+      test1: {
+        pattern: 'search'
+      , replacement: 'replace'
+      , path: 'test.txt'
+      }
+    , test2: {
+        pattern: /\d\d\d\d/
+      , replacement: 'endOfWorld'
+      , path: 'test.txt'
+      }
+    , test3: {
+        pattern: 'LOWERCASE'
+      , replacement: function(match) { return match.toLowerCase(); }
+      , path: 'test.txt'
+      }
+    }
+  });
+
+  grunt.loadTasks('../tasks');
+};
