da64be58cb64eb235fd918b49697e8000393f37b
XhmikosR <xhmikosr@users.sourceforge.net>
2014-04-07 18:58:58 +0300

    Move JSHint's config to .jshintrc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,30 +2,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       options: {
-      // enforcing options
-        curly: true
-      , forin: true
-      , indent: 2
-      , maxlen: 80
-      , newcap: true
-      , noarg: true
-      , noempty: true
-      , nonew: true
-      , quotmark: true
-      , undef: true
-      , unused: true
-
-      // relaxing options
-      , boss: true
-      , expr: true
-      , laxcomma: true
-
-      // environments
-      , node: true
+        jshintrc: '.jshintrc'
       }
+    , gruntfile: ['Gruntfile.js']
     , tasks: ['tasks/*.js']
     , tests: ['test/*.js']
-    , gruntfile: ['Gruntfile.js']
     },
 
     exec: {
